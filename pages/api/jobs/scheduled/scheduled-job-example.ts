c909e03a96698f6ba25815ebe952b22e5753f635
Dwayne Yuen <dwayneyuen@gmail.com>
2022-06-09 21:48:33 -0700

    feat: make every hour and also enqueue something

--- pages/api/jobs/scheduled/scheduled-job-example.ts
+++ pages/api/jobs/scheduled/scheduled-job-example.ts
@@ -1,7 +1,9 @@
 import { ScheduledJob } from "@dwayneyuen/next-jobs";
+import QueueExample from "../queues/queue-example";
 
-const ScheduledJobExample = ScheduledJob("* * * * *", () => {
+const ScheduledJobExample = ScheduledJob("0 * * * *", async () => {
   console.log("[ScheduledJobExample.callback] callback executed");
+  await QueueExample.enqueue({ foo: "bar" });
   return "Hello world!";
 });
 
